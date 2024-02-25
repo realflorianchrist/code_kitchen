a0d50bb69fe75880cc6991db1da4923adfafc1a5
Florian Christ <florian.christ@students.fhnw.ch>
2024-02-25 10:27:31 +0100

    added style and animations

---
+++ personal_website/src/main/scripts/main.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const slideInElements = document.querySelectorAll('.hidden');
+slideInElements.forEach((element) => observer.observe(element));
+
+
