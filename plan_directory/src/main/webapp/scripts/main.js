45e616ae19466cdd80d06b014554f24a1c7873fa
Florian Christ <florian.christ@students.fhnw.ch>
2024-03-01 18:51:54 +0100

    tried to fix filter bar error (1h)

--- plan_directory/src/main/webapp/scripts/main.js
+++ plan_directory/src/main/webapp/scripts/main.js
@@ -1,24 +1,22 @@
+let filterBar = document.getElementById("filterBar");
+let filterButton = document.getElementById("filter-button");
+let saveButton = document.getElementById("save-button");
+
+
 function toggleFilterBar() {
-    let filterBar = document.getElementById("filterBar");
     filterBar.style.display = (filterBar.style.display === "none") ? "block" : "none";
 }
 
-let filterButton = document.querySelector(".filter-button");
 filterButton.addEventListener("click", toggleFilterBar);
 
 function saveButtonPressed() {
     toggleFilterBar();
 }
 
-let saveButton = document.getElementById("save-button");
 saveButton.addEventListener("click", saveButtonPressed);
 
-
-// document.addEventListener("click", function(event) {
-//     let filterBar = document.getElementById("filterBar");
-//     let filterButton = document.querySelector(".filter-button");
-//
-//     if (!filterBar.contains(event.target) && event.target !== filterButton) {
-//         filterBar.style.display = "none";
-//     }
-// });
+document.addEventListener("click", function(event) {
+    if (!filterBar.contains(event.target) && event.target !== filterButton) {
+        filterBar.style.display = "none";
+    }
+});
