fdfaf18ede9c142d62687188fb23b0e98f55d79c
Florian Christ <florian.christ@students.fhnw.ch>
2024-03-02 14:06:28 +0100

    started roman numerals task

---
+++ weekly_tasks/src/RomanNumerals.js
@@ -0,0 +1,23 @@
+const ROMAN_NUMERALS = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+};
+
+function convertRomanNumeralsToNumber(numeral) {
+    let split_numeral = [...numeral];
+    let value = 0;
+
+    split_numeral.forEach(s => {
+
+    });
+
+
+    return value;
+}
+
+console.log(convertRomanNumeralsToNumber("IV"));
