420aa57687de68dc39bdabe874e25902b56f2ca2
Florian Christ <florian.christ@students.fhnw.ch>
2024-03-02 07:01:52 +0100

    tried to connect sql server / added table (1h)

--- plan_directory/src/main/webapp/scripts/main.js
+++ plan_directory/src/main/webapp/scripts/main.js
@@ -3,6 +3,32 @@ let filterButton = document.getElementById("filter-button");
 let saveButton = document.getElementById("save-button");
 
 
+// Diesen Teil habe ich zum Teil aus dem Internet
+
+/*
+document.addEventListener('DOMContentLoaded', function () {
+    fetchData();
+});
+
+async function fetchData() {
+    try {
+        const apiUrl = 'http://localhost:3000/deine-api-endpoint/tabelle';
+
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        data.forEach(row => {
+            const newRow = document.createElement('tr');
+            newRow.innerHTML = `<td>${row.plan_nr}</td><td>${row.plan_path}</td>`;
+
+            table.appendChild(newRow);
+        });
+    } catch (error) {
+        console.error('Fehler beim Abrufen der Daten:', error);
+    }
+}
+*/
+
 function toggleFilterBar() {
     filterBar.style.display = (filterBar.style.display === "none") ? "block" : "none";
 }
